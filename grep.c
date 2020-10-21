31393e248a7510b9604d2229aa1698b890b5470e
Alexey Shcherbakov <Shcherbakov.AA@phystech.edu>
2020-10-22 00:42:48 +0300

    Update grep.c

--- grep.c
+++ grep.c
@@ -8,18 +8,20 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define close_pipes for(i = 0; i < n; i++) close(data_pipe[i][0]), close(data_pipe[i][1]);
+
 
 int main(int argc, char *argv[])
 {
-    int data_pipe[2], file = 0, len = 0, num = 0, pip = -1, pip2 = -1, bt = 0;
-    char buf[1] = "0";
-    char ch;
-    struct stat fstatbuf, stpip, stpip2;
+    int data_pipe[1000][2], file = 0, len = 0, i = 0, n = 0, j = -1;
+    struct stat fstatbuf;
+
     if (argc < 2)
     {
         printf("argc < 2\n");
         return 0;
     }
+
     file = open(argv[1], O_RDONLY);
     fstat(file, &fstatbuf);
     if (file < 0 || ((fstatbuf.st_mode & S_IFMT) != S_IFREG))
@@ -27,44 +29,42 @@ int main(int argc, char *argv[])
         printf("File is not regular\n");
         return 0;
     }
-    int i = 0;
+
     char* file_str = (char*)malloc(fstatbuf.st_size-1);
+    char *substr = NULL;
+    pid_t parent = 1;
     read(file, file_str, fstatbuf.st_size-1);
-    i = 0;
-    char *command = NULL;
-    char *remaining = file_str;
-    pid_t child = 1;
-    pipe(data_pipe);
+    for (i = 0; i < fstatbuf.st_size-1; i++)
+        if (file_str[i] == '|')
+            n++;
+    for (i = 0; i < n; i++)
+        pipe(data_pipe[i]);
 
-    if (child != 0)
+    if (parent)
     {
-        while (command = strtok_r(remaining, "|", &remaining))
+        for (j = 0; substr = strtok_r(file_str, "|", &file_str); j++) 
         {
-            child = fork();
-            if (child == 0)
+            parent = fork();
+            if (!parent)
                 break;
-            wait(0);
         }
     }
 
-    
-    if (child == 0)
+    if (!parent)
     {
-        while (1)
-        {
-            dup2(data_pipe[0], 0);
-            dup2(data_pipe[1], 1);
-            int len = strlen(command), i = 0;
-            if (len == 0)
-                break;
-            char** comms = (char**)malloc(len);
-            while(comms[i] = strtok_r(command, " ", &command))
-                i++;
-            execvp(comms[0], comms);
-        }
+        len = strlen(substr);
+        char** comms = (char**)malloc(len);
+        for (i = 0; comms[i] = strtok_r(substr, " ", &substr); i++)
+        if (j > 0)
+            dup2(data_pipe[j-1][0], 0);
+        if (j < n)
+            dup2(data_pipe[j][1], 1);
+        close_pipes;
+        execvp(comms[0], comms);
     }
-    int size = 1;
-    while((read(data_pipe[0], buf, 1)) > 0)
-        printf("%s", buf);
+
+    close_pipes;
+    for (i = 0; i < n + 1; i++)
+        wait(0);
     return 0;
 }
