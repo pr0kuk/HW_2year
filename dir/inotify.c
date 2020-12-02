e15c4e845d02968c0b3fbb6238ffa987ef1e7025
pr0kuk <shcherbakov.aa@phystech.edu>
2020-12-02 22:37:09 +0300

    Added support for big event names

--- dir/inotify.c
+++ dir/inotify.c
@@ -3,34 +3,43 @@
 #include <unistd.h>
 int main(int argc, char* argv[])
 {
-    int in, watch, ret;
+    int in, watch, ret, lenread = 0, q = 0;
     int mask = IN_CREATE | IN_DELETE | IN_MOVE_SELF;
     char buf[4096];
+    char * ptr;
     struct inotify_event* event;
     in = inotify_init();
-    if (in < 0)
+    if (in == -1)
     {
-        printf("inotify was not initialized\n");
+        perror("inotify_init");
         return -1;
     }
     watch = inotify_add_watch(in, argv[1], mask);
-    if (watch < 0)
+    if (watch == -1)
     {
-        printf("watch was not added\n");
+        perror("inotify_add_watch");
         return -1;
     }
-    
-    while (read(in, buf, 4095)>0)
+    printf("inotify was initialized\n");
+    while (1)
     {
-        event = buf;
-        if (event->mask & IN_CREATE)
-            printf("create %s detected\n", event->name);
-        else
+        lenread = read(in, buf, sizeof(buf));
+        if (lenread == -1)
         {
+            perror("read");
+            return -1;
+        }
+        if (lenread == 0)
+            break;
+        for (ptr = buf; ptr < buf + lenread; ptr += sizeof(struct inotify_event) + event->len)
+        {
+            event = (const struct inotify_event *) ptr;
+            if (event->mask & IN_CREATE)
+                printf("creating %s was detected\n", event->name);
             if (event->mask & IN_DELETE)
-                printf("delete %s detected\n", event->name);
-            else if (event->mask & IN_MOVE_SELF)
-                printf("move %s detected\n", event->name);
+                printf("deleting %s was detected\n", event->name);
+            if (event->mask & IN_MOVE_SELF)
+                printf("moving %s was detected\n", argv[1]);
         }
     }
     return 0;
