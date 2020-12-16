a6dcc9c0e3f0054b8dd97f33eb4b3587acf735d4
pr0kuk <shcherbakov.aa@phystech.edu>
2020-12-16 19:05:28 +0300

    All but the first sender are ignored.

--- trans/handler.c
+++ trans/handler.c
@@ -1,50 +1,69 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <signal.h>
-#include <time.h>
-int i = 1, k = 0;
+int i = 1, k = 0, flag = 1;
 int mas[8];
+pid_t pid;
 void shandler_1(int signum)
 {
     mas[k++] = 0;
+    flag = 1;
 }
 
 void shandler_2(int signum)
 {
     mas[k++] = 1;
+    flag = 1;
 }
 
 void handler_stop(int signum)
 {
-    if (signum == SIGTERM)
-        i = 0;
+    i = 0;
+    flag = 1;
+}
+void receiver_stop(int signum)
+{
+    kill(pid, SIGINT);
+    printf("\nreceiving programm was interrupted\n");
+    raise(SIGKILL);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
 
     int j = 0, d = 0;
     char buf[1];
+    sigset_t* sigset = (sigset_t*)malloc(sizeof(sigset_t));
+    sigemptyset(sigset);
+    sigaddset(sigset, SIGCONT);
+    siginfo_t* siginfo = (siginfo_t*)malloc(sizeof(siginfo_t));
+    sigprocmask(SIG_BLOCK, sigset, NULL);
     signal (SIGUSR1, shandler_1);
     signal (SIGUSR2, shandler_2);
     signal (SIGTERM, handler_stop);
-    printf("pid is %d\n", getpid());
-    int fd = open("file.copy", O_RDWR | O_CREAT, 0666);
+    signal (SIGINT, receiver_stop);
+    printf("recpid is %d\n", getpid());
+    int signum = sigwaitinfo(sigset, siginfo);
+    pid = siginfo->si_pid;
+    printf("senpid is %d\n", pid);
+    kill(pid, SIGUSR1);
+    
+    int fd = open("file.copy", O_RDWR | O_CREAT | O_TRUNC, 0666);
     if (fd < 0)
     {
-        printf("File cannot be opened");
-        return 0;
+        perror("open");
+        return -1;
     }
+
     while (i != 0)
     {
+        flag = 0;
+        kill(pid, SIGUSR1);
+        while(flag == 0);
         if (k == 8)
         {
             d = mas[0] + mas[1] * 2 + mas[2] * 4 + mas[3] * 8 + mas[4] * 16 + mas[5] * 32 + mas[6] * 64 + mas[7] * 128;
@@ -52,9 +71,6 @@ int main()
             write(fd, buf, 1);
             k = 0;
         }
-        if (i == 0)
-            break;
-        j++;
     }
     close(fd);
     return 0;
--- trans/main.c
+++ trans/main.c
@@ -1,51 +1,83 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <signal.h>
 #include <time.h>
+int flag = 1, confirmed_sender = 0;
+pid_t pid;
+void handler(int signum)
+{
+    flag = 1;
+}
+void send_stop(int signum)
+{
+    if (confirmed_sender)
+        kill(pid, SIGINT);
+    printf("\nsending programm was interrupted\n");
+    raise(SIGKILL);
+}
+
+
 int main(int argc, char *argv[])
 {
     int c, i;
-    //clock_t start = clock();
+    signal (SIGUSR1, handler);
+    signal (SIGINT, send_stop);
     int mas[8] = {1, 2, 4, 8, 16, 32, 64, 128};
     char buf[1];
     struct stat fstatbuf;
     if (argc < 3)
     {
         printf("argc < 3\n");
-        return 0;
+        return -1;
     }
-    int pid = atoi(argv[2]);
+    pid = atoi(argv[2]);
+    printf("recpid is %d\n", pid);
+    printf("senpid is %d\n", getpid());
+    flag = 0;
+    kill(pid, SIGCONT);
+    while (flag==0);
+    confirmed_sender = 1;
+
     int file = open(argv[1], O_RDONLY);
+    if (file < 0)
+    {
+        perror("open");
+        return -1;
+    }
+
     fstat(file, &fstatbuf);
     if (file < 0 || ((fstatbuf.st_mode & S_IFMT) != S_IFREG))
     {
         printf("File is not regular\n");
         return 0;
     }
+    clock_t start = clock();
     while (read(file, buf, 1) > 0)
     {
         c = (int)buf[0];
-        //printf("%d ", c);
-        for (i = 0; i < 8; i++)
+        i = 0;
+        while (i < 8)
         {
-            if ((c&1) == 0)
-                kill(pid, SIGUSR1);
-            else
-                kill(pid, SIGUSR2);
-            usleep(10);
-            c = c >> 1;
+            if (flag == 1)
+            {
+                flag = 0;
+                if ((c&1) == 0)
+                    kill(pid, SIGUSR1);
+                else
+                    kill(pid, SIGUSR2);
+                c = c >> 1;
+                i++;
+            }
         }
     }
     kill(pid, SIGTERM);
-    //clock_t end = clock();
-    //printf("time of exectuing is %lf seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+    clock_t end = clock();
+    printf("time of copying is %lf seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
+    printf("copying speed is %lf bytes/seconds\n", fstatbuf.st_size/((double)(end-start)/CLOCKS_PER_SEC));
+    close(file);
     return 0;
 }
