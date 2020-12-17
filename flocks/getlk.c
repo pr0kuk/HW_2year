66454da09b29d31aae40cb44f144afdcb40c5c98
pr0kuk <shcherbakov.aa@phystech.edu>
2020-12-17 14:14:29 +0300

    Added support for many locks

--- flocks/getlk.c
+++ flocks/getlk.c
@@ -1,14 +1,21 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 int main(int argc, char** argv)
 {
-    int fd, ret = 0;
+    int fd, ret = 0, i = 0, flag = 0;
     struct flock f1 = {0};
+    f1.l_type = 0;
+    f1.l_whence = SEEK_SET;
+    f1.l_start = 0;
+    f1.l_len = 0;
+    f1.l_pid = 0;
+    struct flock f2 = {0};
     if (argc < 2)
     {
         printf("argc < 2\n");
@@ -20,17 +27,31 @@ int main(int argc, char** argv)
         perror("open");
         return -1;
     }
-    while (fcntl(fd, F_GETLK, &f1) >= 0)
+    struct stat fstatbuf;
+    fstat(fd, &fstatbuf);
+    size_t len = fstatbuf.st_size;
+    while (i < len)
     {
-        if (f1.l_type == F_WRLCK)
-            printf("l_type = F_WRLCK, ");
-        if (f1.l_type == F_RDLCK) 
-            printf("l_type = F_RDLCK, ");
-        if (f1.l_type == F_UNLCK)
-            printf("l_type = F_UNLCK\n");
-        if (f1.l_type != F_UNLCK)
-            printf("l_whence=%d, l_len=%ld\n", f1.l_whence, f1.l_len);
+        //printf("start is %ld\n", f1.l_start);
+        if (fcntl(fd, F_GETLK, &f1) >= 0 && f1.l_type != F_UNLCK)
+        {
+            if (f1.l_type == F_WRLCK)
+                printf("l_type = F_WRLCK, ");
+            if (f1.l_type == F_RDLCK) 
+                printf("l_type = F_RDLCK, ");
+            printf("l_whence=%d, l_start=%ld, l_len=%ld, l_pid=%d\n", f1.l_whence, f1.l_start, f1.l_len, f1.l_pid);
+            flag = 1;
+            i = f1.l_start + f1.l_len;
+            f1.l_len = 0;
+            f1.l_whence = SEEK_SET;
+            f1.l_pid = 0;
+            f1.l_start = i;
+        }
+        else
+            i++, f1.l_start++;
     }
+    if (!flag)
+        printf("l_type = F_UNLCK\n");
     close(fd);
     return 0;
 }
--- flocks/setlk.c
+++ flocks/setlk.c
@@ -11,8 +11,16 @@ int main(int argc, char** argv)
     struct flock f1 = {0};
     f1.l_type = F_WRLCK;
     f1.l_whence = SEEK_SET;
-    f1.l_len = 0;
+    f1.l_start = 1;
+    f1.l_len = 1;
     f1.l_pid = 0;
+    
+    struct flock f2 = {0};
+    f2.l_type = F_WRLCK;
+    f2.l_whence = SEEK_SET;
+    f2.l_start = 5;
+    f2.l_len = 2;
+    f2.l_pid = 0;
     if (argc < 2)
     {
         printf("argc < 2");
@@ -27,7 +35,13 @@ int main(int argc, char** argv)
     ret = fcntl(fd, F_SETLK, &f1);
     if (ret < 0)
     {
-        perror("fcntl, setlk");
+        perror("fcntl 1, setlk");
+        return -1;
+    }
+    ret = fcntl(fd, F_SETLK, &f2);
+    if (ret < 0)
+    {
+        perror("fcntl 2, setlk");
         return -1;
     }
     sleep(1000);
