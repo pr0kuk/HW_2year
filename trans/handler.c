419d7958fdcc3172b90b7986154d0dc6da69a789
pr0kuk <shcherbakov.aa@phystech.edu>
2020-12-18 02:43:10 +0300

    Speed boosted using sigqueue

--- trans/handler.c
+++ trans/handler.c
@@ -5,73 +5,78 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <signal.h>
-int i = 1, k = 0, flag = 1;
-int mas[8];
+int i = 1, flag = 1, ignition = 0;
+void* mem;
 pid_t pid;
-void shandler_1(int signum)
+void shandler_1(int sig, siginfo_t *si, void *ucontext)
 {
-    mas[k++] = 0;
-    flag = 1;
-}
-
-void shandler_2(int signum)
-{
-    mas[k++] = 1;
+    memcpy(mem, &(si->si_value.sival_ptr), sizeof(void*));
     flag = 1;
 }
 
 void handler_stop(int signum)
 {
     i = 0;
-    flag = 1;
 }
+
 void receiver_stop(int signum)
 {
     kill(pid, SIGINT);
     printf("\nreceiving programm was interrupted\n");
     raise(SIGKILL);
 }
-
+void killrest(int signum, siginfo_t *siginfo, void* context)
+{
+    if (pid != 0 && siginfo->si_pid != pid)
+    {
+        kill(siginfo->si_pid, SIGKILL);
+        printf("sender %d was killed\n", siginfo->si_pid);
+    }
+    else
+    {
+        ignition = 1;
+        pid = siginfo->si_pid;
+        printf("senpid is %d\n", siginfo->si_pid);
+    }
+}
 int main(int argc, char* argv[])
 {
 
-    int j = 0, d = 0;
-    char buf[1];
-    sigset_t* sigset = (sigset_t*)malloc(sizeof(sigset_t));
-    sigemptyset(sigset);
-    sigaddset(sigset, SIGCONT);
-    siginfo_t* siginfo = (siginfo_t*)malloc(sizeof(siginfo_t));
-    sigprocmask(SIG_BLOCK, sigset, NULL);
-    signal (SIGUSR1, shandler_1);
-    signal (SIGUSR2, shandler_2);
+    mem = malloc(sizeof(void*));
+    char temp[1];
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = shandler_1;
+    sa.sa_flags = SA_SIGINFO;
+    sigaction(SIGUSR1, &sa, NULL);
     signal (SIGTERM, handler_stop);
     signal (SIGINT, receiver_stop);
     printf("recpid is %d\n", getpid());
-    int signum = sigwaitinfo(sigset, siginfo);
-    pid = siginfo->si_pid;
-    printf("senpid is %d\n", pid);
-    kill(pid, SIGUSR1);
-    
+    struct sigaction killing;
+    killing.sa_flags = SA_SIGINFO;
+    killing.sa_sigaction = &killrest;
+    sigaction(SIGCHLD, &killing, NULL);
     int fd = open("file.copy", O_RDWR | O_CREAT | O_TRUNC, 0666);
     if (fd < 0)
     {
         perror("open");
         return -1;
     }
+    while (ignition == 0);
 
-    while (i != 0)
+
+    while (1)
     {
         flag = 0;
         kill(pid, SIGUSR1);
         while(flag == 0);
-        if (k == 8)
-        {
-            d = mas[0] + mas[1] * 2 + mas[2] * 4 + mas[3] * 8 + mas[4] * 16 + mas[5] * 32 + mas[6] * 64 + mas[7] * 128;
-            buf[0] = d;
-            write(fd, buf, 1);
-            k = 0;
-        }
+        if (i == 0)
+            break;
+        write(fd, (char*)mem, sizeof(void*));
     }
+    
+    write(fd, (char*)mem, strlen((char*)mem));
+    kill(pid, SIGUSR1);
     close(fd);
     return 0;
 }
--- trans/main.c
+++ trans/main.c
@@ -23,11 +23,11 @@ void send_stop(int signum)
 
 int main(int argc, char *argv[])
 {
-    int c, i;
+    int ret = sizeof(void*);
+    union sigval sv;
     signal (SIGUSR1, handler);
     signal (SIGINT, send_stop);
-    int mas[8] = {1, 2, 4, 8, 16, 32, 64, 128};
-    char buf[1];
+    char buf[sizeof(void*)];
     struct stat fstatbuf;
     if (argc < 3)
     {
@@ -38,44 +38,41 @@ int main(int argc, char *argv[])
     printf("recpid is %d\n", pid);
     printf("senpid is %d\n", getpid());
     flag = 0;
-    kill(pid, SIGCONT);
-    while (flag==0);
+    kill(pid, SIGCHLD);
+    while (flag == 0);
     confirmed_sender = 1;
-
     int file = open(argv[1], O_RDONLY);
     if (file < 0)
     {
         perror("open");
         return -1;
     }
-
     fstat(file, &fstatbuf);
     if (file < 0 || ((fstatbuf.st_mode & S_IFMT) != S_IFREG))
     {
         printf("File is not regular\n");
         return 0;
     }
+
+
     clock_t start = clock();
-    while (read(file, buf, 1) > 0)
+    while (ret == sizeof(void*))
     {
-        c = (int)buf[0];
-        i = 0;
-        while (i < 8)
+        ret = read(file, buf, sizeof(void*));
+        flag = 0;
+        if (ret < sizeof(void*))
         {
-            if (flag == 1)
-            {
-                flag = 0;
-                if ((c&1) == 0)
-                    kill(pid, SIGUSR1);
-                else
-                    kill(pid, SIGUSR2);
-                c = c >> 1;
-                i++;
-            }
+            kill(pid, SIGTERM);
+            buf[ret] = 0;
         }
+        memcpy(&(sv.sival_ptr), buf, sizeof(void*));
+        sigqueue(pid, SIGUSR1, sv);
+        while (flag == 0);
     }
     kill(pid, SIGTERM);
     clock_t end = clock();
+
+    
     printf("time of copying is %lf seconds\n", (double)(end-start)/CLOCKS_PER_SEC);
     printf("copying speed is %lf bytes/seconds\n", fstatbuf.st_size/((double)(end-start)/CLOCKS_PER_SEC));
     close(file);
